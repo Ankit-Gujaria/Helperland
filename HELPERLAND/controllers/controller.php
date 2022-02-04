76bca5c8d52824e4f8aa21a353f61a002d889522
Ankit-Gujaria <ankitgujaria2000@gmail.com>
2022-02-04 14:12:41 +0530

    controller chanches

--- HELPERLAND/controllers/controller.php
+++ HELPERLAND/controllers/controller.php
@@ -11,36 +11,9 @@
           $this->usermodel=new Helperland();
       }
 
-     
-
-   }
-
-
-   public function contactUs(){
-    $contact = [
-        'Name' => $_POST['FirstName']. " " .$_POST['LastName'],
-        'Email' => $_POST['Email'],
-        'sub'=>$_POST['sub'],
-        'phone'=>$_POST['phone'],
-        'msg'=>$_POST['msg']
-    ];
-
-    // Validate inputs
-    if(empty($contact['Name']) || empty($contact['Email']) || empty($contact['phone']) || empty($contact['sub']) || empty($contact['msg']))
-    {
-            flash('contact', 'Please fill out all inputs');
-            redirect('../views/ContactUs.php');
-    }
-    // Message sent succesfull or not
-    if($this->usermodel->contact($contact)){
-        $_SESSION['mesaage'] = $contact['msg'];
-        flash('succesmsg','Your message sent successfully','form-message form-message-green text-center text-success');
-        redirect("../views/ContactUs.php");
-    }
-    else
-    {
-        echo "<h1>Message not sent</h1>";
-    }
+      public fuction HomePage()
+      {
+          include('view/index.php');
 
 
 
@@ -72,7 +45,7 @@
 		{
                 flash("register", "For registering Please fill out all inputs");
             redirect('../views/CreateAccount.php');
-		}
+      }
 
 
 		if(!filter_var($data['usersEmail'],FILTER_VALIDATE_EMAIL)){
@@ -184,112 +157,9 @@ public function serviceprovider_Register(){
         flash("register" , "Registration succesfull");
         redirect("../views/upcoming_service.php");
     }
-    else
-    {
-        echo "<h1>Account is not created ,please fill form proper</h1>";
-    }
-
-}
-
-public function login(){
-        
-
-    //Init data
-    $data=[
-        'Email' => trim($_POST['Email']),
-        'usersPwd' => trim($_POST['password'])
-    ];
-
-    if(empty($data['Email']) || empty($data['usersPwd'])){
-        flash("login", "Please fill out all inputs");
-        header("location: ../views/index.php");
-        exit();
-    }
-
-    //Check for user/email
-    if($this->usermodel->findUserByEmailOrUsername($data['Email'], $data['Email'])){
-        //User Found
-        $loggedInUser = $this->usermodel->login($data['Email'], $data['usersPwd']);
-        if($loggedInUser){
-            //Create session
-            $this->createUserSession($loggedInUser);
-        }else{
-            flash("login", "Password Incorrect");
-            redirect("../views/index.php");
-        }
-    }else{
-        flash("login", "No user found");
-        redirect("../views/index.php");
-    }
-}
-public function createUserSession($user){
     
-    $_SESSION['FirstName'] = $user;
-    redirect("../views/customer_service_history.php");
-}
-
-public function logout(){
-    unset($_SESSION['FirstName']);
-    session_destroy();
-    redirect("../views/index.php");
-
-}
-}
-$init = new Controller;
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
-switch ($_POST['type']){
-     case 'contact' :
-     $init->contactUs();
-     break;
-
-     case 'register' :
-     $init->customerRegister();
-     break;
-
-     case 'service_provider' :
-     $init->serviceprovider_Register();
-     break;
-
-     case 'login' :
-     $init->login();
-     break;
-     default:
-     redirect("../views/index.php");
-}
-}
-else
-{
-switch($_GET['q']){
-    case 'home' :
-        redirect("../views/index.php");
-        break;
-    case 'about' :
-        redirect("../views/About.php");
-        break;
-    case 'faq' :
-        redirect("../views/FAQs.php");
-        break;
-    case 'contactus' : 
-        redirect("../views/ContactUs.php");
-        break;
-    case 'prices' :
-        redirect("../views/Prices.php");
-        break;
-    case 'serviceprovider' :
-        redirect("../views/Become_a_Pro.php");
-        break;
-    case 'logout' :
-        $init->logout();
-        break;
-    default:
-        redirect("../views/index.php");
-
-}
-}
-
 
 
 }
-
 
 ?>
